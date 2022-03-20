33521dbf54246e28b052598baba16769a5f1ed1a
mahasri1824 <101971191+mahasri1824@users.noreply.github.com>
2022-03-20 15:49:18 +0530

    created to identify the file
    helps to identify the python type files.

---
+++ file.py
@@ -0,0 +1,7 @@
+a=input("Input the file name:")
+type=["py","cpp","c","js"]
+filename=["python","other files"]
+if(type[0] in a):
+    print("the extension of the file is:",filename[0])
+else:
+    print("the extension of the file is:",filename[1])
